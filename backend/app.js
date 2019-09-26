d66a8ce262a57781211ab5c204bfb7ce0c2bb505
joshua marieta <josmarieta@gmail.com>
2019-09-26 23:05:13 +0300

    First commit

---
+++ backend/app.js
@@ -0,0 +1,128 @@
+//MONGODB PW : 8eAHDpqlCwxxsOcj
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Thing = require('./model/thing');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://Jomario2:8eAHDpqlCwxxsOcj@cluster0-dtgsw.mongodb.net/test?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(bodyParser.json());
+//post one thing
+app.post('/api/recipes', (req, res, next) => {
+    const thing = new Thing({
+      title: req.body.title,
+      ingredients: req.body.ingredients,
+      instructions: req.body.instructions,
+      difficulty: req.body.difficulty,
+      time: req.body.time
+    });
+    thing.save().then(
+      () => {
+        res.status(201).json({
+          message: 'Post saved successfully!'
+        });
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+});
+
+//get route to retutn all things
+app.use('/api/recipes', (req, res, next) => {
+    Thing.find().then(
+      (things) => {
+        res.status(200).json(things);
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+});
+
+//get route with a specific id
+app.get('/api/recipes/:id', (req, res, next) => {
+    Thing.findOne({
+      _id: req.params.id
+    }).then(
+      (thing) => {
+        res.status(200).json(thing);
+      }
+    ).catch(
+      (error) => {
+        res.status(404).json({
+          error: error
+        });
+      }
+    );
+});
+
+//modify with put
+app.put('/api/recipes/:id', (req, res, next) => {
+    const thing = new Thing({
+        _id: req.params.id,
+        title: req.body.title,
+        ingredients: req.body.ingredients,
+        instructions: req.body.instructions,
+        difficulty: req.body.difficulty,
+        time: req.body.time
+      });
+  Thing.updateOne({_id: req.params.id}, thing).then(
+    () => {
+      res.status(201).json({
+        message: 'Thing updated successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+//delete
+app.delete('/api/recipes/:id', (req, res, next) => {
+    Thing.deleteOne({_id: req.params.id}).then(
+      () => {
+        res.status(200).json({
+          message: 'Deleted!'
+        });
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  });
+
+
+
+
+
+module.exports = app;
